25abc61e682b52273521d3657388f01aae3b4171
MichalSlepicka123 <misiakslepica@gmail.com>
2025-04-10 21:38:19 +0200

    Update MenuController.cs

--- Assets/SKRIPTY/MenuController.cs
+++ Assets/SKRIPTY/MenuController.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject infoPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("LEVEL1");
+    }
+
+    public void ShowInfo()
+    {
+        infoPanel.SetActive(true);
+    }
+
+    public void HideInfo()
+    {
+        infoPanel.SetActive(false);
     }
 }
